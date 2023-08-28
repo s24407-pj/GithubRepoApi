a5ca22feb87721726e820b07e197b5bd5f58a3f5
Michal <s24407@pjwstk.edu.pl>
2023-08-28 17:29:38 +0200

    Initial commit

---
+++ src/main/java/com/example/githubrepoapi/GithubRepoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubrepoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubrepoapi/GithubRepoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubrepoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
