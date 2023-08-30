1a550c6f22ff5fc678656736a21db107fee6515e
Michal <s24407@pjwstk.edu.pl>
2023-08-30 19:33:02 +0200

    create bean for webclient with base GitHub api url

---
+++ src/main/java/com/example/githubrepoapi/config/WebClientConfig.java
@@ -0,0 +1,13 @@
+package com.example.githubrepoapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    WebClient webClient() {
+        return WebClient.create("https://api.github.com");
+    }
+}
